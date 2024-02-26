b2276d2086fec7575aae8e7db4ab1d3f19a2b6af
georgekazz <sfinas1998@hotmail.com>
2024-02-26 12:02:24 +0200

    add models

---
+++ app/Models/Link.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Admin\Controllers\FileController;
+use EasyRdf\RdfNamespace;
+
+class Link extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['source_id', 'target_id', 'source_entity', 'target_entity', 'user_id'];
+    protected $dates = ['deleted_at'];
+
+    public function project()
+    {
+        return $this->belongsTo("App\Models\Project");
+    }
+
+    public function user()
+    {
+        return $this->belongsTo("App\Models\User");
+    }
+
+    public function source()
+    {
+        return $this->hasOne('App\Models\File', 'id', 'source_id');
+    }
+
+
+    public function target()
+    {
+        return $this->hasOne('App\Models\File', 'id', 'target_id');
+    }
+
+    public function votes()
+    {
+        return $this->hasMany('App\Models\Vote');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    // public function myVote(){
+    //     try{
+    //         $vote = \App\Models\Vote::where("user_id", "=", auth()->user()->id)->where("link_id", "=", $this->id)->firstOrFail();
+    //     }
+    //     catch (ModelNotFoundException $ex){
+    //         $vote = null;
+    //     }
+    //     return $vote;
+    // }
+
+    public function humanize()
+    {
+        $project = \App\Models\Project::find($this->attributes["project_id"]);
+        $file = new FileController();
+        $file->cacheGraph(\App\Models\File::find($project->source_id));
+        $file->cacheGraph(\App\Models\File::find($project->target_id));
+        $source_graph = \Illuminate\Support\Facades\Cache::get($project->source_id . "_graph");
+        $target_graph = \Illuminate\Support\Facades\Cache::get($project->target_id . "_graph");
+        $ontologies_graph = \Illuminate\Support\Facades\Cache::get('ontologies_graph');
+        $source_label = \App\Models\RDFTrait::label($source_graph, $this->source_entity) ?: RdfNamespace::shorten($this->source_entity, true);
+        $this->source_label = $source_label;
+        $target_label = \App\Models\RDFTrait::label($target_graph, $this->target_entity) ?: RdfNamespace::shorten($this->target_entity, true);
+        $this->target_label = $target_label;
+        $link_label = \App\Models\RDFTrait::label($ontologies_graph, $this->link_type) ?: RdfNamespace::shorten($this->link_type, true);
+        $this->link_label = $link_label;
+        $vote = $this->myVote();
+        $this->myvote = $vote != null ? $vote->vote : null;
+        return $this;
+    }
+}
---
+++ app/Models/RDFTrait.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cookie;
+use EasyRdf\RdfNamespace;
+use EasyRdf\Graph;
+
+class RDFTrait extends Model
+{
+    use HasFactory;
+
+    public static function setNamespaces()
+    {
+        $namespaces = \App\Models\rdfnamespace::where('added', '=', '1')->get();
+        foreach ($namespaces as $namespace) {
+            RdfNamespace::set($namespace->prefix, $namespace->uri);
+        }
+        return 0;
+    }
+
+    public static function uknownNamespace($uri)
+    {
+        $tempnamespace = new \EasyRdf\Resource($uri);
+        $local = $tempnamespace->localName();
+
+        $namespace = mb_substr($uri, 0, -mb_strlen($local));
+        $existing = \App\Models\rdfnamespace::where('uri', '=', $namespace)->get();
+        if ($existing->isEmpty()) {
+            \App\Models\rdfnamespace::create([
+                'prefix' => 'null',
+                'uri' => $namespace,
+                'added' => 0
+            ]);
+        }
+        return $uri;
+    }
+
+    public static function mergeGraphs(Graph $graph1, Graph $graph2)
+    {
+        $data1 = $graph1->toRdfPhp();
+        $data2 = $graph2->toRdfPhp();
+        $merged = array_merge_recursive($data1, $data2);
+        unset($data1, $data2);
+        return new Graph('urn:easyrdf:merged', $merged, 'php');
+    }
+
+    public static function label(Graph $graph, $uri)
+    {
+        $label_properties =
+            \App\Models\LabelExtractor::where('enabled', '=', '1')
+                ->orderBy('priority', 'asc')
+                ->get();
+        $label = null;
+        $locale = Cookie::get('locale');
+        foreach ($label_properties as $property) {
+            if ($label == null) {
+                $label = $graph
+                    ->getLiteral(
+                        $uri,
+                        new \EasyRdf\Resource($property->property),
+                        $locale
+                    );
+            } else {
+                break;
+            }
+            if ($label == null) {
+                //get default label in English. This should be configurable on .env
+                $label = $graph
+                    ->getLiteral(
+                        $uri,
+                        new \EasyRdf\Resource($property->property),
+                        'en'
+                    );
+            }
+            if ($label == null) {
+                //if no english label found try a label in any language
+                $label = $graph
+                    ->getLiteral(
+                        $uri,
+                        new \EasyRdf\Resource($property->property)
+                    );
+            }
+        }
+        if ($label == null) {
+            $label = RdfNamespace::shorten($uri, true);
+        }
+        return $label;
+    }
+}
