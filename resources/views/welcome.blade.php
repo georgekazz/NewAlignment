ac71a522768edf0f7a9a197192a4bcd4a60f94b9
georgekazz <sfinas1998@hotmail.com>
2025-05-29 15:10:06 +0300

    fix ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,8 @@
     <title>Alignment OKFN - Greece</title>
 
     <!-- Favicons -->
-    <link rel="icon" href="../public/img/favicon-alignment.png">
-    <link rel="apple-touch-icon" href="../public/img/favicon-alignment.png">
+    <link rel="icon" href="./img/favicon-alignment.png">
+    <link rel="apple-touch-icon" href="./img/favicon-alignment.png">
 
     <!-- Google Fonts -->
     <link href="//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic" rel="stylesheet"
@@ -28,52 +28,42 @@
     <!-- Main CSS File -->
     <link href="./css/style.css" rel="stylesheet">
     <link href="./css/custom.css" rel="stylesheet">
-
-    <!-- Toastify CSS -->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 </head>
 
 <style>
-    /* Προσαρμογή διαστάσεων των εικόνων στο carousel */
     #screenshotCarousel .carousel-item img {
         max-height: 600px;
-        /* Μέγιστο ύψος */
         max-width: 600px;
-        /* Μέγιστο πλάτος */
         margin: auto;
-        /* Κεντράρισμα της εικόνας */
     }
 
     .carousel-control-prev-icon,
     .carousel-control-next-icon {
         background-color: orange;
-        /* Πορτοκαλί χρώμα */
     }
 
     .carousel-caption {
-        background-color: rgba(0, 0, 0, 0.8); /* Μαύρο ημι-διαφανές φόντο */
-        padding: 10px; /* Εσωτερικό περιθώριο */
-        border-radius: 5px; /* Στρογγυλεμένες γωνίες */
-        bottom: 20px; /* Απόσταση από το κάτω μέρος της εικόνας */
+        background-color: rgba(0, 0, 0, 0.8); 
+        padding: 10px; 
+        border-radius: 5px; 
+        bottom: 20px; 
     }
 
     .carousel-caption p {
         margin: 0;
-        color: white; /* Χρώμα κειμένου λευκό */
-        font-size: 1rem; /* Μέγεθος γραμματοσειράς */
+        color: white; 
+        font-size: 1rem; 
     }
 </style>
 
 <body>
 
-    <!-- ======= Header ======= -->
     <header id="header" class="fixed-top d-flex align-items-center  header-transparent ">
         <div class="container d-flex align-items-center justify-content-between">
 
             <div class="logo">
                 <h1><a href="#">Alignment</a></h1>
-                <!-- Uncomment below if you prefer to use an image logo -->
-                <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
             </div>
 
             <nav id="navbar" class="navbar">
@@ -92,12 +82,10 @@
 
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
-            </nav><!-- .navbar -->
-
+            </nav>
         </div>
-    </header><!-- End Header -->
+    </header>
 
-    <!-- ======= Hero Section ======= -->
     <section id="hero" class="d-flex flex-column justify-content-end align-items-center">
         <div id="heroCarousel" data-bs-interval="5000" class="container carousel carousel-fade" data-bs-ride="carousel">
 
@@ -127,22 +115,18 @@
                 <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
             </g>
         </svg>
-
-    </section><!-- End Hero -->
+    </section>
 
     <main id="main">
-
         <!-- About Section -->
         <section id="about" class="about">
             <div class="container">
 
-                <!-- About Section Title -->
                 <div class="section-title" data-aos="zoom-out">
                     <h2>About</h2>
                     <p>Ontology matching</p>
                 </div>
 
-                <!-- About Section Content -->
                 <div class="row content" data-aos="fade-up">
                     <div class="col-lg-6">
                         <p>
@@ -150,27 +134,22 @@
                             and other distributed, open world applications. Diversity in tools,
                             knowledge, habits, language, interests and usually level of detail
                             may drive in heterogeneity. Thus, many automated applications have
-                            <!-- Αφαιρέθηκε ο επιπλέον HTML κώδικας εδώ -->
                         </p>
                     </div>
                 </div>
-
             </div>
-        </section><!-- End About Section -->
+        </section>
 
         <!-- Services Section -->
         <section id="services" class="services">
             <div class="container">
 
-                <!-- Section Title -->
                 <div class="section-title" data-aos="zoom-out">
                     <h2>Services</h2>
                     <p>Build to Connect</p>
                 </div>
 
-                <!-- Services Items -->
                 <div class="row">
-                    <!-- Service Item 1 -->
                     <div class="col-lg-4 col-md-6">
                         <div class="icon-box" data-aos="zoom-in-left">
                             <div class="icon"><i class="bi bi-people"></i></div>
@@ -179,7 +158,6 @@
                                 features.</p>
                         </div>
                     </div>
-                    <!-- Service Item 2 -->
                     <div class="col-lg-4 col-md-6 mt-5 mt-md-0">
                         <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="100">
                             <div class="icon"><i class="bi bi-gear"></i></div>
@@ -187,7 +165,6 @@
                             <p class="description">Configure Alignment easily through intuitive panels.</p>
                         </div>
                     </div>
-                    <!-- Service Item 3 -->
                     <div class="col-lg-4 col-md-6 mt-5 mt-lg-0">
                         <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="200">
                             <div class="icon"><i class="bi bi-link-45deg"></i></div>
@@ -197,26 +174,19 @@
                         </div>
                     </div>
                 </div>
-
             </div>
-        </section><!-- End Services Section -->
-
-
-
+        </section>
 
         <!-- Team Section -->
         <section id="team" class="team">
             <div class="container">
 
-                <!-- Section Title -->
                 <div class="section-title" data-aos="zoom-out">
                     <h2>Our Team</h2>
                     <p>Meet Our Team</p>
                 </div>
 
-                <!-- Team Members -->
                 <div class="row">
-                    <!-- Team Member 1 -->
                     <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
                         <div class="member" data-aos="fade-up">
                             <div class="member-img">
@@ -226,33 +196,27 @@
                             </div>
                         </div>
                     </div>
-                    <!-- Repeat the above structure for additional team members -->
                 </div>
 
             </div>
-        </section><!-- End Team Section -->
-
+        </section>
 
         <!-- Screenshot Section -->
         <section id="screenshot" class="screenshot">
             <div class="container">
 
-                <!-- Section Title -->
                 <div class="section-title" data-aos="zoom-out">
                     <h2>Screenshots</h2>
                 </div>
 
-                <!-- Carousel -->
                 <div id="screenshotCarousel" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
-                        <!-- Screenshot 1 -->
                         <div class="carousel-item active">
                             <img src="{{ asset('img/gui.jpg') }}" class="d-block w-100" alt="GUI Screenshot">
                             <div class="carousel-caption d-none d-md-block">
                                 <p>New Admin Panel</p>
                             </div>
                         </div>
-                        <!-- Screenshot 2 -->
                         <div class="carousel-item">
                             <img src="{{ asset('img/flowchart.jpg') }}" class="d-block w-100"
                                 alt="Flowchart Screenshot">
@@ -260,7 +224,6 @@
                                 <p>Brand new layout for alignment</p>
                             </div>
                         </div>
-                        <!-- Screenshot 3 -->
                         <div class="carousel-item">
                             <img src="{{ asset('img/checked_venn.jpg') }}" class="d-block w-100"
                                 alt="Checked Venn Screenshot">
@@ -268,7 +231,6 @@
                                 <p>New page for graphs</p>
                             </div>
                         </div>
-                        <!-- Screenshot 4 -->
                         <div class="carousel-item">
                             <img src="{{ asset('img/chart-img.jpg') }}" class="d-block w-100"
                                 alt="Checked Venn Screenshot">
@@ -277,7 +239,6 @@
                             </div>
                         </div>
                     </div>
-                    <!-- Carousel Navigation -->
                     <button class="carousel-control-prev" type="button" data-bs-target="#screenshotCarousel"
                         data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -291,10 +252,7 @@
                 </div>
 
             </div>
-        </section><!-- End Screenshot Section -->
-
-
-
+        </section>
 
         <!-- ======= Contact Section ======= -->
         <section id="contact" class="contact">
@@ -323,9 +281,8 @@
                 </div>
 
             </div>
-        </section><!-- End Contact Section -->
-
-    </main><!-- End #main -->
+        </section>
+    </main>
 
     <!-- ======= Footer ======= -->
     <footer id="footer">
@@ -343,12 +300,11 @@
                 </p>
             </div>
         </div>
-    </footer><!-- End Footer -->
+    </footer>
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Vendor JS Files -->
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('vendor/aos/aos.js') }}"></script>
     <script src="{{ asset('vendor/glightbox/js/glightbox.min.js') }}"></script>
@@ -356,7 +312,6 @@
     <script src="{{ asset('vendor/swiper/swiper-bundle.min.js') }}"></script>
     <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
 
-    <!-- Template Main JS File -->
     <script src="{{ asset('js/main.js') }}"></script>
 
 </body>
